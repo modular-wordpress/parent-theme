c8246a84a9718a0facca8396d4d807f54c34f883
Ivan Mattie <mattieivan@gmail.com>
2018-02-05 12:26:52 -0700

    Testing commit

--- src/page.php
+++ src/page.php
@@ -15,6 +15,7 @@
 get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			Hello, world.
 			<?php
 				if(have_rows("content_modules")) {
 					while(have_rows("content_modules")) {
