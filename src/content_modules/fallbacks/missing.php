ba7256c2e3b9f3aedec3fdb42bf969bb1d4c762d
Ivan Mattie <mattieivan@gmail.com>
2018-02-05 13:07:36 -0700

    Fleshed out missing template some more

--- src/content_modules/fallbacks/missing.php
+++ src/content_modules/fallbacks/missing.php
@@ -1,1 +1,4 @@
-Content Module Missing.
+<section class="content-module missing-content-module fallback-module">
+    No Content Module Found for Layout
+    <span class="layout-name"><?= get_row_layout() ?></span>
+</section>
